7ecd11882f582ce980e0348355ed179da83d9431
Bhavya2065 <sanjivdani1@gmail.com>
2026-07-02 23:47:01 +0530

    user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,7 +57,7 @@ userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function(password) {
+userSchema.methods.generateAccessToken = function() {
     return jwt.sign(
         {
             _id: this._id,
@@ -72,7 +72,7 @@ userSchema.methods.generateAccessToken = function(password) {
     )
 }
 
-userSchema.methods.generateRefreshToken = function(password) {
+userSchema.methods.generateRefreshToken = function() {
     return jwt.sign(
         {
             _id: this._id

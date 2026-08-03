fb79f4f8c54b97d8f763b5e40a837f66255b7755
Bhavya2065 <sanjivdani1@gmail.com>
2026-08-03 20:18:25 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -17,11 +17,13 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import commentRouter from './routes/comment.routes.js'
 
 // Routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/subscription", subscriptionRouter)
+app.use("/api/v1/comments", commentRouter)
 
 export default app
 

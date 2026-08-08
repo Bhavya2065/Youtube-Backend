57321fe6d8dc5f82c158984c56d7e991bb3441ae
Bhavya2065 <sanjivdani1@gmail.com>
2026-08-08 19:18:04 +0530

    tweet router

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { addPost, deletePost, editPost, getAllPost, viewPost } from "../controllers/tweet.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT)
+
+// Tweet Routes
+router.route("/create-post").post(addPost)
+router.route("/view-post/:postId").get(viewPost)
+router.route("/get-post").get(getAllPost)
+router.route("/edit-post/:postId").patch(editPost)
+router.route("/delete-post/:postId").delete(deletePost)
+
+export default router;
